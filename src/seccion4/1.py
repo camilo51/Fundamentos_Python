7b0385fc90ad4a87b218cbc4402b210c322ec9e2
Cristian Pereira <pereiracamilo51@gmail.com>
2026-07-23 10:21:37 -0500

    Seccion 4 -} Variables

---
+++ src/seccion4/1.py
@@ -0,0 +1,24 @@
+var = 1
+account_balance = 500000.0
+client_name = 'Cristian Pereira'
+print(var, account_balance, client_name)
+print(var)
+
+print()
+
+var = "3.14.2"
+print("Python version: " + var)
+
+print()
+
+var = 1
+print(var)
+var = var + 1
+print(var)
+
+print()
+
+a = 3.0
+b = 4.0
+c = (a ** 2 + b ** 2) ** 0.5
+print("c =", c)
