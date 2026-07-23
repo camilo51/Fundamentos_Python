223f4208fdd1945cdf4ca7ebba207305619903b8
Cristian Pereira <pereiracamilo51@gmail.com>
2026-07-23 09:59:04 -0500

    Seccion 3 - Operadores

---
+++ src/seccion3/1.py
@@ -0,0 +1,30 @@
+# Suma
+print(2+2)
+
+# Exponente
+print(2 ** 3)
+print(2 ** 3.)
+print(2. ** 3)
+print(2. ** 3.)
+
+# Multiplicación
+print(2 * 3)
+print(2 * 3.)
+print(2. * 3)
+print(2. * 3.)
+
+# División
+print(6 / 3)
+print(6 / 3.)
+print(6. / 3)
+print(6. / 3.)
+
+# División entera
+print(6 // 3)
+print(6 // 3.)
+print(6. // 3)
+print(6. // 3.)
+
+# Módulo
+print(14 % 4)
+print(12 % 4.5)
