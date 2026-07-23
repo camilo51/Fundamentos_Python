debe6d97ea86916595bc2dec6ba9ff5fac19cdbb
Cristian Pereira <pereiracamilo51@gmail.com>
2026-07-23 09:50:34 -0500

    Seccion 2 - Literales de Python

---
+++ src/seccion2/1.py
@@ -0,0 +1,23 @@
+# Tipo str y int 
+print(type("2"))
+print(type(2))
+
+# Exadecimales
+print(0o123)
+print(0xc)
+print(0x123)
+
+# Potencia
+print(3E8)
+print(6.62607E-34)
+
+# Enteros y flotantes
+print(4)
+print(.4)
+print(4.)
+
+# Exponencial
+print(0.0000000000000000000001)
+
+print(True > False)
+print(True < False)
